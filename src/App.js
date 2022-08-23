48cfbc9f8518cc05113100d23469695575c74da8
Maksim Ivanovskiy <ivanovskiy.maxim@mail.ru>
2022-08-23 19:59:49 -0400

    optimization

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
         super();
 
         this.state = {
-            monsters: []
+            monsters: [],
+            searchField: ''
         };
     }
 
@@ -24,26 +25,31 @@ class App extends Component {
         ))
     }
 
+    onSearchChange = (event) => {
+        const searchField = event.target.value.toLowerCase();
+
+        this.setState(() => {
+            return { searchField };
+        }); 
+    }
+
     render() {
+        const { monsters, searchField } = this.state;
+        const { onSearchChange } = this;
+        const filteredMonsters = monsters.filter((monster) => {
+            return monster.name.toLowerCase().includes(searchField)
+        });
+
         return (
             <div className="App">
                 <input 
                     className='search-box' 
                     type='search' 
                     placeholder='search monsters' 
-                    onChange={(event) => {
-                        const searchString = event.target.value.toLowerCase();
-                        const filteredMonsters = this.state.monsters.filter((monster) => {
-                            return monster.name.toLowerCase().includes(searchString)
-                        });
-
-                        this.setState(() => {
-                            return { monsters: filteredMonsters };
-                        });
-                    }}
+                    onChange={onSearchChange}
                 />
                 {
-                    this.state.monsters.map((monster) => {
+                    filteredMonsters.map((monster) => {
                         return <div key={monster.id}><h1>{monster.name}</h1></div>
                     })
                 }
